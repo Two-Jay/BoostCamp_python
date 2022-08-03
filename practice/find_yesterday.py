5c25c4a1183446f05c140408b1727a287241d523
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-08-03 14:16:35 +0900

    yesterday_finder done

---
+++ practice/find_yesterday.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+
+def request_lyrics():
+    url = "https://raw.githubusercontent.com/TeamLab/introduction_to_python_TEAMLAB_MOOC/master/code/6/yesterday.txt"
+    return urlopen(url).read().decode("utf-8")
+
+
+def find_word_in_lyrics(word, lyrics):
+    print(f"{word} count: {lyrics.lower().count(word)}")
+
+print("Yesterday ___ by Beatles ____")
+print(request_lyrics())
+
+target_str = input("Yesterday 가사에서 찾을 단어를 입력하세요 (소문자로) : ")
+find_word_in_lyrics(target_str, request_lyrics())
+
